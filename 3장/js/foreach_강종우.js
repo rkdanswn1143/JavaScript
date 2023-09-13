a6527f7145dadc01c621dd19fd30898e16148f92
rkdanswn1143 <67546978+rkdanswn1143@users.noreply.github.com>
2023-09-13 15:08:48 +0900

    Add files via upload

---
+++ 3장/js/foreach_강종우.js
@@ -0,0 +1,5 @@
+const students = ["이 준","박민재","차승헌","신동재","김재윤"]
+
+students.forEach(function(student) {
+    document.write(`${student}<br>`)
+});
---
+++ 3장/js/three.js
@@ -0,0 +1,9 @@
+
+let three = parseFloat(prompt("3의배수 계산할거 입력"))
+
+if(three%3==0){
+    alert(`입력하신 숫자 ${three} 은 3의 배수 입니다`)
+}
+else{
+    alert(`입력하신 숫자${three}3의 배수가 아닙니다`)
+}
